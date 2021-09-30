16dc8b585560240df259ec073359d2ab2b77bf79
Joel Bx <87010768+Cinemavillaz@users.noreply.github.com>
2021-09-30 12:22:02 +0530

    Update __init__.py

--- LuciferMoringstar_Robot/__init__.py
+++ LuciferMoringstar_Robot/__init__.py
@@ -11,33 +11,9 @@ from .Channel import (
 )
 
 HELP = """
-Basic Commads
-
-/start :- check am Alive
-
-Bot Owner Only
-/broadcast Replay Any Message or Media
-/stats user Status
-/ban_user  click ban_user more info
-/unban_user click unban_user more info
-/banned_users Banned User Details
-/total How Many Files Added In Database
-/logger  Get Logs
-/delete Delete File From Database
-
-[➡️How To Add Old Files For database](https://youtu.be/mtu5hgW-l_o)
-
-@Mo_Tech_YT
+ഇത്രെയും വലുതായില്ലേ പിന്നെ എന്തിനാ സഹായം 😐
 """
 
 ABOUT = """
-➥ Name :  Auto Filter With IMDB
-    
-➥ Creator : [Muhammed RK](t.me/Mrk_YT)
-
-➥ Language : Python3
-
-➥ Library : Pyrogram Asyncio 1.13.0
-
-➥ Source Code : [Click Me](https://github.com/PR0FESS0R-99/LuciferMoringstar_Robot)
+എന്നെ കുറിച്ച് ഒന്നും പറയാനില്ല 😐
 """
